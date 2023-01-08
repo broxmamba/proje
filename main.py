821832ea890f504ea80fa869b765b0e049e8893f
basri-creator <99613717+basri-creator@users.noreply.github.com>
2023-01-08 21:30:42 +0300

    Update main.py
    Değişken adlarının herkes tarafından anlaşılacağı şekilde globalleştirilmesi ve yorum satırlarının eklenmesi.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame as pg
 pg.init()
 
-#create game window
+#SİMİLASYON PENÇERESİNİN YAPILMASI
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 750
 
@@ -9,7 +9,7 @@ screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pg.display.set_caption("Ohm kanunu")
 
 
-#game variables
+#SİMİLASYON DEĞİŞKENLERİ
 game_paused = False
 menu_state = "main"
 volt = 10
@@ -20,10 +20,41 @@ rtoplam = 5.40
 Circuit_State= 1
 Akımson = 1.85
 
-#define fonts
-font = pg.font.SysFont("arialblack", 15)
-fontd = pg.font.SysFont("arialblack", 25)
-fontD = pg.font.SysFont("arial",20)
-#define colours
-TEXT_COL = (255, 255, 255)
-D_COL = (0,0,0)
+#YAZI FONTUNUN TANIMLANMASI
+font_15 = pg.font.SysFont("arialblack", 15)
+font_25 = pg.font.SysFont("arialblack", 25)
+font_20 = pg.font.SysFont("arial",20)
+
+#RENKLERİN TANIMLANMASI
+WHITE_RGB = (255, 255, 255)
+BLACK_RGB= (0,0,0)
+
+#DİRENÇ RESİMLERİNİN YÜKLENMESİ
+direnc = pg.image.load("Direnc.png")
+devre_img1 = pg.image.load("PS.png").convert_alpha()
+devre_img2 = pg.image.load("paralel.png").convert_alpha()
+devre_img3 = pg.image.load("seri.png").convert_alpha()
+devre_img = devre_img1
+
+#BUTON RESİMLERİNİN YÜKLENMESİ
+def dikdörtgen(genislik,yukseklik,x,y): #Dikdörtgen oluşturur
+
+  dikdörtgen_nesnesi = pg.Surface((genislik,yukseklik))
+  dikdörtgen_nesnesi.fill((240, 248, 255))
+  dikdörtgen_nesnesi_konumu = (x, y)
+  
+  screen.blit(dikdörtgen_nesnesi, dikdörtgen_nesnesi_konumu)
+  
+#BUTONUN YÜKLENMESİ
+class Button():
+  def __init__(self, x, y, image, oran):
+	  width = image.get_width()
+		height = image.get_height()
+		self.image = pg.transform.scale(image, (int(width * oran), int(height * oran)))
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x, y)
+		self.clicked = False  
+  
+  
+  
+
