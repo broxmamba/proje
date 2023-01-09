ecc2795de8cfb2c2bd9d74bba97c28ebb00af939
Hakan <117983418+Hakanoto5@users.noreply.github.com>
2023-01-09 16:06:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,24 @@ class Button():
 		self.rect = self.image.get_rect()
 		self.rect.topleft = (x, y)
 		self.clicked = False  
-  
+  def draw(self, surface):
+		action = False
+		#get mouse position
+		pos = pg.mouse.get_pos()
+
+		#check mouseover and clicked conditions
+		if self.rect.collidepoint(pos):
+			if pg.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				self.clicked = True
+				action = True
+
+		if pg.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+
+		#draw button on screen
+		surface.blit(self.image, (self.rect.x, self.rect.y))
+
+		return action 
   
   
 
